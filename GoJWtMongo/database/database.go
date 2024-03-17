15cacda2a1a2beec4d0e9a2feae4711bae3457aa
Nicolas Calvario <nicolas.calvario.gonzalez@gmail.com>
2024-03-17 16:09:54 -0600

    se versionan cambios

---
+++ GoJWtMongo/database/database.go
@@ -0,0 +1,49 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func ConnectDB() *mongo.Client {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error al cargar archivo .env")
+	}
+	MongoDb := os.Getenv("MONGODB_URL")
+	opts := options.Client().ApplyURI(MongoDb)
+	client, err := mongo.Connect(context.TODO(), opts)
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	if err := client.Database("app-test").RunCommand(context.TODO(), bson.D{{Key: "ping", Value: 1}}).Err(); err != nil {
+		panic(err)
+	}
+	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+
+	return client
+}
+
+// Client Database instance
+var Db *mongo.Client = ConnectDB()
+
+// OpenCollection is a  function makes a connection with a collection in the database
+func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+
+	var collection *mongo.Collection = client.Database("app-test").Collection(collectionName)
+
+	return collection
+}
---
+++ GoJWtMongo/routes/userRoutes.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	controller "api-jwt-mongo/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+//UserRoutes function
+func UserRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.POST("/users/signup", controller.SignUp())
+	incomingRoutes.POST("/users/login", controller.Login())
+}
